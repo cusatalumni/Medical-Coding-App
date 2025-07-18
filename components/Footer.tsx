fe9219a046ad9248494701aa71701cb9c3647056
cusatalumni <cusatalumni@gmail.com>
2025-07-18 10:52:58 +0400

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,8 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-white mt-auto">
       <div className="container mx-auto px-4 py-4 text-center text-slate-500">
-        <p>&copy; {new Date().getFullYear()} Medical Coding Online. All Rights Reserved.  An <a  href="https://annapoornainfo.com"  target="_blank"  rel="noopener noreferrer"  > Annapoorna Infotech </a> Venture</p>
+        <p>&copy; {new Date().getFullYear()} Medical Coding Online. All Rights Reserved.  
+          An <a  href="https://annapoornainfo.com"  target="_blank"  rel="noopener noreferrer"  > Annapoorna Infotech </a> Venture</p>
         
       
       </div>
